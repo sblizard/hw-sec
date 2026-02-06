7e94602bb5cfab57b3512630459dd7d327667622
AlexSieni <asieni@unc.edu>
2026-02-06 15:55:28 -0500

    Done

--- lab-websitefingerprinting/part2/eval.py
+++ lab-websitefingerprinting/part2/eval.py
@@ -1,11 +1,12 @@
 import json
 import numpy as np
+import sys
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
-def eval():
+def eval(path="traces.out"):
     y_pred_full, y_test_full = [], []
 
     # Re-train 10 times in order to reduce effects of randomness
@@ -15,6 +16,15 @@ def eval():
         ### 2. Split data into X_train, X_test, y_train, y_test with train_test_split
         ### 3. Train classifier with X_train and y_train
         ### 4. Use classifier to make predictions on X_test. Save the result to a variable called y_pred
+        with open(path, "r") as f:
+            data = json.load(f) 
+        X = data["traces"]
+        y = data["labels"]
+ 
+        X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2)
+        model = RandomForestClassifier()
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
 
         # Do not modify the next two lines
         y_test_full.extend(y_test)
@@ -22,6 +32,7 @@ def eval():
 
     ### TODO: Exercise 2-5 (continued)
     ### 5. Print classification report using y_test_full and y_pred_full
+    print(classification_report(y_test_full, y_pred_full))
 
 if __name__ == "__main__":
-    eval()
+    eval(path="traces.out")
--- lab-websitefingerprinting/part2/worker.js
+++ lab-websitefingerprinting/part2/worker.js
@@ -1,6 +1,7 @@
 // Number of sweep counts
 // TODO (Exercise 2-1): Choose an appropriate value!
-let P = 1000;
+let P = 25;
+let N = 32768;
 
 // Number of elements in your trace
 let K = 5 * 1000 / P; 
@@ -22,6 +23,18 @@ function record() {
   start = performance.now();
 
   // TODO (Exercise 2-1): Record data for 5 seconds and save values to T.
+  M = new Array(N * 16).fill(-1);
+  for (let i = 0; i < K; i++){
+    count = 0;
+    P_init = performance.now();
+    while (performance.now() < P_init + P){
+      for (let line = 0; line < M.length / 16; line++){
+        let val = M[16*line];
+      }
+      count += 1;
+    }
+    T[i] = count;
+  }
 
   // Once done recording, send result to main thread
   postMessage(JSON.stringify(T));
