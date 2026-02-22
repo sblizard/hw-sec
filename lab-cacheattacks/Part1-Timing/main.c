740f54792bc5fbe3630d7ad0bcf6bb95edac259d
AndrewZmijewski <122329776+AndrewZmijewski@users.noreply.github.com>
2026-02-22 11:07:28 -0500

    eliminating segmentation fault

--- lab-cacheattacks/Part1-Timing/main.c
+++ lab-cacheattacks/Part1-Timing/main.c
@@ -79,7 +79,7 @@ int main (int ac, char **av) {
         tmp = target_buffer[0];
 
         // Step 2: Evict the entire L1 cache so that the program is forced to access the target address from L2
-        for (int j=0; j<49152; j+=100){
+        for (int j=0; j<6144; j++){
            tmp = l2_eviction_buffer[j];
         }
       
