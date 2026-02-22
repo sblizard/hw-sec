525db476b3048674417065c968a6a863b7ccbe00
AndrewZmijewski <122329776+AndrewZmijewski@users.noreply.github.com>
2026-02-22 09:43:53 -0500

    Fixing print statement

--- lab-cacheattacks/Part1-Timing/main.c
+++ lab-cacheattacks/Part1-Timing/main.c
@@ -80,7 +80,7 @@ int main (int ac, char **av) {
 
         // Step 2: Evict the entire L1 cache so that the program is forced to access the target address from L2
         tmp = l2_eviction_buffer[0];
-        fprint(sizeof(l2_eviction_buffer[0]));
+        printf(sizeof(l2_eviction_buffer[0]));
       
         // Step 3: measure the access latency
         l2_latency[i] = measure_one_block_access_time((uint64_t)target_buffer);
