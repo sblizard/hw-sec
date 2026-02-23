0dc0ec758bad3fb820490f350e6df9b437460ed6
AndrewZmijewski <122329776+AndrewZmijewski@users.noreply.github.com>
2026-02-23 10:50:34 -0500

    Update sender.c

--- lab-cacheattacks/Part2-DeadDrop/sender.c
+++ lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -40,7 +40,6 @@ int main(int argc, char **argv)
   while(true) {
     while (sending) {
       char text_buf[1];
-      printf("test");
       fgets(text_buf, sizeof(text_buf), stdin);
       sending = false;
       // TODO:
