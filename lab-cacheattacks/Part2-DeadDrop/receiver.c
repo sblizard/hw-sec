5b5607ab8f836b83adab29bce36767cd0ce03aa7
AndrewZmijewski <122329776+AndrewZmijewski@users.noreply.github.com>
2026-02-23 11:41:18 -0500

    signal in receiver

--- lab-cacheattacks/Part2-DeadDrop/receiver.c
+++ lab-cacheattacks/Part2-DeadDrop/receiver.c
@@ -7,6 +7,13 @@ int main(int argc, char **argv)
 {
 	// Put your covert channel setup code here
 
+	uint64_t *eviction_buffer = (uint64_t *)malloc(1.5*512*8*sizeof(uint64_t));
+  
+  	if (NULL == eviction_buffer) {
+      perror("Unable to malloc");
+      return EXIT_FAILURE;
+  	}
+	
 	printf("Please press enter.\n");
 
 	char text_buf[2];
@@ -24,6 +31,10 @@ int main(int argc, char **argv)
 	printf("Receiver finished.\n");
 
 	return 0;
+}
+
+void handle_signal(int signal_num) {
+    printf("Received signal in receiver: %d\n", signal_num);
 }
 
 
