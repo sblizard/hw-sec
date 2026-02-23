58d60cb7cf1812428dcc1ec480c29cde876eb33a
AndrewZmijewski <122329776+AndrewZmijewski@users.noreply.github.com>
2026-02-23 10:23:15 -0500

    first try sender setup

--- lab-cacheattacks/Part2-DeadDrop/sender.c
+++ lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -4,28 +4,35 @@
 #include <sys/mman.h>
 
 // TODO: define your own buffer size
-#define BUFF_SIZE (1<<21)
+//#define BUFF_SIZE (1<<21)
 //#define BUFF_SIZE [TODO]
 
 int main(int argc, char **argv)
 {
   // Allocate a buffer using huge page
   // See the handout for details about hugepage management
-  void *buf= mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
+  //void *buf= mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
   
-  if (buf == (void*) - 1) {
-     perror("mmap() error\n");
-     exit(EXIT_FAILURE);
-  }
+  //if (buf == (void*) - 1) {
+    // perror("mmap() error\n");
+     //exit(EXIT_FAILURE);
+  //}
   // The first access to a page triggers overhead associated with
   // page allocation, TLB insertion, etc.
   // Thus, we use a dummy write here to trigger page allocation
   // so later access will not suffer from such overhead.
-  *((char *)buf) = 1; // dummy write to trigger page allocation
+  //*((char *)buf) = 1; // dummy write to trigger page allocation
 
 
   // TODO:
   // Put your covert channel setup code here
+
+  uint64_t *eviction_buffer = (uint64_t *)malloc(1.5*512*8*sizeof(uint64_t));
+  
+  if (NULL == l2_eviction_buffer) {
+      perror("Unable to malloc");
+      return EXIT_FAILURE;
+  }
 
   printf("Please type a message.\n");
 
@@ -36,9 +43,13 @@ int main(int argc, char **argv)
 
       // TODO:
       // Put your covert channel code here
-      measure_one_block_access_time(&buf[0]);
-      *buf[0] = text_buf;  
-      measure_one_block_access_time(&buf[0]);
+
+      if(text_buf == 1) {
+        for (int j=0; j<6144; j++){
+           eviction_buffer[j] = 1;
+        }
+      }
+    
       sending = false;
   }
 
