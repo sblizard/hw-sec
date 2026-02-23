bb40766775fb13d5ea52df4a132f67062a52b2b3
AndrewZmijewski <122329776+AndrewZmijewski@users.noreply.github.com>
2026-02-23 11:14:53 -0500

    newline characters

--- lab-cacheattacks/Part2-DeadDrop/sender.c
+++ lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -44,9 +44,9 @@ int main(int argc, char **argv)
       sending = false;
       // TODO:
       // Put your covert channel code here
-      printf("%ld", (uint64_t)string_to_binary(text_buf));
-      printf("%ld", (uint64_t)binary_to_string(text_buf));
-      printf("%ld", (uint64_t)string_to_int(text_buf));
+      printf("%ld\n", (uint64_t)string_to_binary(text_buf));
+      printf("%ld\n", (uint64_t)binary_to_string(text_buf));
+      printf("%ld\n", (uint64_t)string_to_int(text_buf));
       if((uint64_t)string_to_binary(text_buf) == 1) {
         printf("input was 1");
         for (int j=0; j<6144; j++){
