6547da84a83e4e1bba55deec984daf70cf9b17c2
AndrewZmijewski <122329776+AndrewZmijewski@users.noreply.github.com>
2026-02-24 14:13:28 -0500

    Update sender.c

--- lab-cacheattacks/Part2-DeadDrop/sender.c
+++ lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
   ftruncate(mem, 1);
   void *m = mmap(NULL, 1, PROT_READ | PROT_WRITE, MAP_SHARED, mem, 0);
   &m = 1;
-  printf("%d\n", m);
+  printf("%p\n", m);
   
   //if (buf == (void*) - 1) {
     // perror("mmap() error\n");
