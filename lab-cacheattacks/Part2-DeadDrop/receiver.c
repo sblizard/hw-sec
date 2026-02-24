de7a3aeda114092f44d73b6506907aa2a42ded6d
AndrewZmijewski <122329776+AndrewZmijewski@users.noreply.github.com>
2026-02-24 13:03:44 -0500

    cleaning up receiver code

--- lab-cacheattacks/Part2-DeadDrop/receiver.c
+++ lab-cacheattacks/Part2-DeadDrop/receiver.c
@@ -2,7 +2,6 @@
 #include"util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
-#include <math.h>
 
 int main(int argc, char **argv)
 {
@@ -10,13 +9,7 @@ int main(int argc, char **argv)
 
 	//create a buffer
 	uint64_t *eviction_buffer = (uint64_t *)malloc(1.5*16384*8*sizeof(uint64_t));
-	uint64_t *test_buffer = (uint64_t *)malloc(1.5*16384*8*sizeof(uint64_t));
 	uint64_t *t_buffer = (uint64_t *)malloc(8*sizeof(uint64_t));
-	//uint64_t *u_buffer = (uint64_t *)malloc(8*sizeof(uint64_t));
-	//uint64_t *v_buffer = (uint64_t *)malloc(8*sizeof(uint64_t));
-	//uint64_t *w_buffer = (uint64_t *)malloc(8*sizeof(uint64_t));
-	//uint64_t *x_buffer = (uint64_t *)malloc(8*sizeof(uint64_t));
-
 
   	if (NULL == eviction_buffer) {
       perror("Unable to malloc");
@@ -35,46 +28,30 @@ int main(int argc, char **argv)
 	bool listening = true;
 
 	while (listening) {
+		
 		// Put your covert channel code here
-		// Fill the L2 cache with the buffer
-		/*for (int j=0; j<6144; j++){
-           eviction_buffer[j] = 1;
-        }*/
 		int result = 0;
+
+		// Prime the L2 cache with the buffer
 		for(int k = 0; k < 8; k++) {
 			int count = 0;
 			for(int i = 0; i < 100; i++) {
-			tmp = t_buffer[0];
-			//tmp = u_buffer[0];
-			//tmp = v_buffer[0];
-			//tmp = w_buffer[0];
-			//tmp = x_buffer[0];
-			//t_buffer[0] = 1;
+				tmp = t_buffer[0];
+	
 			//wait for signal
-			if((string_to_int(text_buf) >> k) & 0b00000001 == 0b1) {
-				printf("Test\n");
-				for (int j=0; j<196608; j+=4){
-           			test_buffer[j] = 1;
-        		}
-			}
-			//time access to eviction buffer
-			CYCLES time = measure_one_block_access_time((uint64_t)t_buffer);
-			//CYCLES uime = measure_one_block_access_time((uint64_t)u_buffer);
-			//CYCLES vime = measure_one_block_access_time((uint64_t)v_buffer);
-			//CYCLES wime = measure_one_block_access_time((uint64_t)w_buffer);
-			//CYCLES xime = measure_one_block_access_time((uint64_t)x_buffer);
 
-			//printf("%d\n", time);
-			//printf("%d\n", uime);
-			//printf("%d\n", vime);
-			//printf("%d\n", wime);
-			//printf("%d\n", xime);
-			//printf("%d\n", time+uime+vime+wime+xime);
-			//int sum = time + uime + vime + wime + xime;
-			printf("%d\n", time);
-			if((time > 130 && time < 250) || (time > 310)) {
-				count++;
-			}
+			//Determine bits sequentially by evicting the L2 cache on 1's
+				if((string_to_int(text_buf) >> k) & 0b00000001 == 0b1) {
+					for (int j=0; j<196608; j+=4){
+           				eviction_buffer[j] = 1;
+        			}
+				}
+			//time access to eviction buffer
+				CYCLES time = measure_one_block_access_time((uint64_t)t_buffer);
+
+				if((time > 130 && time < 250) || (time > 310)) {
+					count++;
+				}
 			}
 			if(count >= 50) {
 				int exponent = k+1;
@@ -97,10 +74,7 @@ int main(int argc, char **argv)
 	free(eviction_buffer);
 	free(test_buffer);
 	free(t_buffer);
-	//free(u_buffer);
-	//free(v_buffer);
-	//free(w_buffer);
-	//free(x_buffer);
+
 	return 0;
 }
 
