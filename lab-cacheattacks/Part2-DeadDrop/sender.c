363d5025dd723499ac8a7fd0d798a5a8bd1c26fd
Sean Patrick Blizard <sblizard@burnsville.cs.unc.edu>
2026-02-24 16:33:51 -0500

    sender.c helpers

--- lab-cacheattacks/Part2-DeadDrop/sender.c
+++ lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -4,8 +4,51 @@
 #include <sys/mman.h>
 
 // TODO: define your own buffer size
-#define BUFF_SIZE (1<<21)
-//#define BUFF_SIZE [TODO]
+#define CACHE_LINE       64
+#define L2_SETS          1024
+#define L2_WAYS          16
+#define NUM_SECTIONS     8
+#define SETS_PER_SECT    (L2_SETS / NUM_SECTIONS)    
+#define LINES_PER_SECT   (SETS_PER_SECT * L2_WAYS)    
+#define BYTES_PER_SECT   (LINES_PER_SECT * CACHE_LINE)
+
+#define EVICT_OFFSET     L2_SIZE  
+
+#define BUFF_SIZE        (1 << 21) 
+
+public void prime_section(volatile char *buf, int section) 
+{
+	volatile char *base = buf + (section*BYTES_PER_SECT); //Find base address for section
+	//Access every line in cache section
+	for (int i = 0; i < BYTES_PER_SECT; i += CACHE_LINE) {
+        	(void)base[i];	
+	}
+}
+
+public void evict_section(volatile char *buf, int section) 
+{
+	//Get base address for evicting cache lines
+	volatile char *base = buf + EVICT_OFFSET + (section*BYTES_PER_SECT);
+	//Access an address for each cache line
+	for (int i = 0; i < BYTES_PER_SECT; i += CACHE_LINE) {
+        	(void)base[i];	
+	}
+}
+
+uint64_t probe_section(volatile char *buf, int section) 
+{
+	//Base address for section
+	volatile char *base = buf + (section * BYTES_PER_SECT);
+	//Coutner for amount of time needed to make n line accesses in section
+	uint64_t total = 0;
+    	for (int i = 0; i < BYTES_PER_SECT; i += CACHE_LINE) {
+        	uint64_t t1 = rdtsc_begin();
+        	(void)base[i];
+        	uint64_t t2 = rdtsc_end();
+        	total += (t2 - t1);
+    	}
+	return total / LINES_PER_SECT;
+}
 
 int main(int argc, char **argv)
 {
@@ -26,7 +69,7 @@ int main(int argc, char **argv)
 
   // TODO:
   // Put your covert channel setup code here
-
+  uint64_t *buffer = (uint64_t *)malloc(BUFF_SIZE);
   printf("Please type a message.\n");
 
   bool sending = true;
