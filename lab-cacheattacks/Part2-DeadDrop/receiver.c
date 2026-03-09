f0209470bb8907824c4df812adbac99a043eda6e
Sean Blizard <emailseanblizard@gmail.com>
2026-03-09 15:45:28 -0400

    Teamwork

--- lab-cacheattacks/Part2-DeadDrop/receiver.c
+++ lab-cacheattacks/Part2-DeadDrop/receiver.c
@@ -1,62 +1,69 @@
 
-#include"util.h"
+#include "util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
-#include"covert.h"
+#include "covert.h"
 
 int main(int argc, char **argv)
 {
 	void *buf = mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
-    	if (buf == (void *)-1) { 
-		perror("mmap() error"); 
-		exit(EXIT_FAILURE); 
+	if (buf == (void *)-1)
+	{
+		perror("mmap() error");
+		exit(EXIT_FAILURE);
 	}
 	printf("buf: %p\n", buf);
-	for (size_t i = 0; i < BUFF_SIZE; i += 4096) {
-        	((volatile char *)buf)[i] = 1;
+	for (size_t i = 0; i < BUFF_SIZE; i += 4096)
+	{
+		((volatile char *)buf)[i] = 1;
 	}
-	
+
 	printf("Please press enter.\n");
 
 	char text_buf[2];
 	fgets(text_buf, sizeof(text_buf), stdin);
 
 	printf("Receiver now listening.\n");
-	
+
 	int freq[256] = {0};
 	bool listening = true;
-	while (listening) {
-		for (int bit = 0; bit < 8; bit ++) {
+	while (listening)
+	{
+		for (int bit = 0; bit < 8; bit++)
+		{
 			prime_section(buf, bit);
 		}
 
-		for (volatile int i = 0; i < 100000; i++);
-		
+		for (volatile int i = 0; i < 100000; i++)
+			;
+
 		uint8_t value = 0;
-        	for (int bit = 0; bit < 8; bit++) {
+		for (int bit = 0; bit < 8; bit++)
+		{
 			uint64_t t = probe_section(buf, bit);
-        		if (t > MISS_THRESHOLD) {
-            			value |= (1 << bit);
+			if (t > MISS_THRESHOLD)
+			{
+				value |= (1 << bit);
 			}
-    		}
-        	
+		}
+
 		fflush(stdout);
-		if (value != 0) {
+		if (value != 0)
+		{
 			freq[value]++;
 		}
 		int mode = 0;
-        	
+
 		for (int i = 1; i < 256; i++)
-            	
-		if (freq[i] > freq[mode]) { 
-			mode = i;
-		}
-        	printf("%d\n", mode);
-        	fflush(stdout);
+
+			if (freq[i] > freq[mode])
+			{
+				mode = i;
+			}
+		printf("%d\n", mode);
+		fflush(stdout);
 	}
 
 	printf("Receiver finished.\n");
 	return 0;
 }
-
-
--- lab-cacheattacks/Part2-DeadDrop/sender.c
+++ lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -1,18 +1,19 @@
 
-#include"util.h"
+#include "util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
-#include"covert.h"
+#include "covert.h"
 
 int main(int argc, char **argv)
 {
   // Allocate a buffer using huge page
   // See the handout for details about hugepage management
-  void *buf= mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
-  
-  if (buf == (void*) - 1) {
-     perror("mmap() error\n");
-     exit(EXIT_FAILURE);
+  void *buf = mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
+
+  if (buf == (void *)-1)
+  {
+    perror("mmap() error\n");
+    exit(EXIT_FAILURE);
   }
 
   printf("buf: %p\n", buf);
@@ -22,33 +23,36 @@ int main(int argc, char **argv)
   // so later access will not suffer from such overhead.
   *((char *)buf) = 1; // dummy write to trigger page allocation
 
-
-  for (size_t i = 0; i < BUFF_SIZE; i += 4096) {
-        ((volatile char *)buf)[i] = 1;
+  for (size_t i = 0; i < BUFF_SIZE; i += 4096)
+  {
+    ((volatile char *)buf)[i] = 1;
   }
   printf("Please type a message.\n");
 
   bool sending = true;
-  while (sending) {
-      char text_buf[128];
-      fgets(text_buf, sizeof(text_buf), stdin);
+  while (sending)
+  {
+    char text_buf[128];
+    fgets(text_buf, sizeof(text_buf), stdin);
 
-      int val = atoi(text_buf);
-      if (val < 0 || val > 255) {
-      	printf("Please enter 0-255.\n");
-        continue;
+    int val = atoi(text_buf);
+    if (val < 0 || val > 255)
+    {
+      printf("Please enter 0-255.\n");
+      continue;
+    }
+
+    while (true)
+    {
+      for (int bit = 0; bit < 8; bit++)
+      {
+        if ((val >> bit) & 1)
+        {
+          evict_section(buf, bit);
+        }
       }
-      
-      while(true){  
-      	for (int bit = 0; bit < 8; bit++) {
-          if ((val >> bit) & 1) {
-            evict_section(buf, bit);
-          }
-	}
-      }
+    }
   }
 
   return 0;
 }
-
-
